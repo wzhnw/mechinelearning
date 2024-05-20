4ab45fe478847fce4c8f38e3f94f4131b6d1662c
wzhnw <166518406+wzhnw@users.noreply.github.com>
2024-05-20 17:05:46 +0800

    Update ipykernel_launcher.py

--- ipykernel_launcher.py
+++ ipykernel_launcher.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Set page title and icon
-st.set_page_config(page_title="RSF Prediction App", page_icon=":bar_chart:")
+st.set_page_config(page_title="RSF Prediction App for the Overall Survival of Pediatric Wilms' Tumor", page_icon=":bar_chart:")
 
 # Add a title and subheader
-st.title("RSF Prediction App")
+st.title("RSF Prediction App for the Overall Survival of Pediatric Wilms' Tumor")
 st.subheader("Enter patient information and click Submit to get the prediction")
 
 # Create a sidebar for input fields
 with st.sidebar:
     st.header("Input Information")
-    Stage = st.number_input("Stage")
+    Stage = st.selectbox("Stage", [1, 2, 3, 4])
     Age = st.number_input("Age")
     Distant_Metastasis = st.checkbox("Distant Metastasis")
     Preoperative_Chemotherapy = st.checkbox("Preoperative Chemotherapy")
@@ -23,25 +25,32 @@ with st.sidebar:
 
 # If button is pressed
 if submit_button:
-    # Load the saved model from the file
-    
-    # Load the saved model from the file
-    with open('./rsf.pkl', 'rb' ) as f:
-        clf = pickle.load(f)
+    try:
+        # Load the saved model from the file
+        with open('./rsf.pkl', 'rb') as f:
+            clf = pickle.load(f)
 
-    
-    # Store inputs into dataframe
-    X = pd.DataFrame([[Perirenal_Fat_Invasion, Preoperative_tumor_rupture, 
-                       Preoperative_Chemotherapy, Stage, Age,
-                       Distant_Metastasis, Radiotherapy]], 
-                     columns=['Perirenal_Fat_Invasion', 'Preoperative_tumor_rupture', 
-                              'Preoperative_Chemotherapy', 'Stage', 'Age',
-                              'Distant_Metastasis', 'Radiotherapy'])
-    
-    # Get prediction
-    prediction = clf.predict(X)[0]
-    
-    # Output prediction
-    st.success(f"This instance is predicted as: {prediction}")
+        # Store inputs into dataframe
+        X = pd.DataFrame([[Perirenal_Fat_Invasion, Preoperative_tumor_rupture, 
+                           Preoperative_Chemotherapy, Stage, Age,
+                           Distant_Metastasis, Radiotherapy]], 
+                         columns=['Perirenal_Fat_Invasion', 'Preoperative_tumor_rupture', 
+                                  'Preoperative_Chemotherapy', 'Stage', 'Age',
+                                  'Distant_Metastasis', 'Radiotherapy'])
+
+        surv = clf.predict_survival_function(X, return_array=True)
+
+        for i, s in enumerate(surv):
+            plt.step(range(125), s, where="post", label=str(i))
+        plt.ylabel("Survival probability")
+        plt.xlabel("Survival time (mo)")
+        plt.legend(loc='upper right', labels=['' for _ in range(len(surv))] )
+        plt.grid(True)
+
+        # Output survival probabilities
+        st.pyplot(plt)
+        st.success("Survival probabilities plotted successfully.")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
    
